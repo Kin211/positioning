a9053bd983b7327957b276e74cfee8ca0650bb3e
Kin <kin234142@gmail.com>
2026-04-07 18:41:46 +0500

    до 4

--- index.js
+++ index.js
@@ -5,6 +5,21 @@
     element.style.color = 'red';
     element.style.width = '300px';
 */
+
+const load = document.querySelector('.prog-load');
+
+function startAnimation() {
+    let width = 0;
+    const timer = setInterval(() => {
+        width++; // Прибавляем 1%
+        load.style.width = width + '%';
+
+        if (width >= 100) {
+            clearInterval(timer);
+        }
+    }, 30);
+}
+
 function buttonFunction(event) {
     let target = event.target;
 
@@ -15,6 +30,8 @@ function buttonFunction(event) {
     else if (target.classList.contains('lightbox__open')) {
         let lightbox = document.querySelector('.lightbox');
         lightbox.style.display = 'block';
+        startAnimation();
+        load.style.width = "0%"
     }
 }
 
@@ -23,3 +40,6 @@ let openButton = document.querySelector('.lightbox__close');
 openButton.addEventListener('click', buttonFunction);
 let closeButton = document.querySelector('.lightbox__open');
 closeButton.addEventListener('click', buttonFunction);
+
+
+
